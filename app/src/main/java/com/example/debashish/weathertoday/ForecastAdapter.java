e2ccda848534bf5c0e93240f409b90db9504dcdd
Dinesh Kumar Mohanty <dineshkumarmohanty.111@gmail.com>
2017-07-08 14:43:46 +0530

    Some polish made to the UI .

--- app/src/main/java/com/example/debashish/weathertoday/ForecastAdapter.java
+++ app/src/main/java/com/example/debashish/weathertoday/ForecastAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.debashish.weathertoday.utils.TempratureUtils;
@@ -42,21 +43,43 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     @Override
     public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
-
         mCursor.moveToPosition(position);
+
+        int weatherId = mCursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
+        int weatherImageId;
+
+        weatherImageId = TempratureUtils
+                .getSmallArtResourceIdForWeatherCondition(weatherId);
+
+       holder.iconView.setImageResource(weatherImageId);
+
+
         long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
         String dateString = WeatherDateUtils.getFriendlyDateString(mContext, dateInMillis, false);
-        int weatherId = mCursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
         String description = TempratureUtils.getStringForWeatherCondition(mContext, weatherId);
+
+
         double highInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP);
         double lowInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP);
+        String highString = TempratureUtils.formatTemperature(mContext, highInCelsius);
+        String highA11y = mContext.getString(R.string.a11y_high_temp, highString);
 
-        String highAndLowTemperature =
-               TempratureUtils.formatHighLows(mContext, highInCelsius, lowInCelsius);
+         /* Set the text and content description (for accessibility purposes) */
+       holder.highTempView.setText(highString);
+        holder.highTempView.setContentDescription(highA11y);
 
-        String weatherSummary = dateString + " - " + description + " - " + highAndLowTemperature;
 
-        holder.mWeatherTextView.setText(weatherSummary);
+
+        String lowString = TempratureUtils.formatTemperature(mContext, lowInCelsius);
+        String lowA11y = mContext.getString(R.string.a11y_low_temp, lowString);
+
+         /* Set the text and content description (for accessibility purposes) */
+       holder.lowTempView.setText(lowString);
+       holder.lowTempView.setContentDescription(lowA11y);
+
+
+        holder.dateView.setText(dateString);
+        holder.descriptionView.setText(description);
 
     }
 
@@ -68,11 +91,19 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
 
     public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        public final  TextView mWeatherTextView;
+        final TextView dateView;
+        final TextView descriptionView;
+        final TextView highTempView;
+        final TextView lowTempView;
+        final ImageView iconView;
 
         public ForecastAdapterViewHolder(View view) {
             super(view);
-            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+            iconView = (ImageView) view.findViewById(R.id.weather_icon);
+            dateView = (TextView) view.findViewById(R.id.date);
+            descriptionView = (TextView) view.findViewById(R.id.weather_description);
+            highTempView = (TextView) view.findViewById(R.id.high_temperature);
+            lowTempView = (TextView) view.findViewById(R.id.low_temperature);
             view.setOnClickListener(this);
         }
 
